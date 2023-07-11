ba85e4e7ef2097620eb28d11e12c06342d180230
asuka-higa <asuka.higa4study@gmail.com>
2023-07-12 03:18:36 +0900

    7/12　いったん完成

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use strict'
 
+// ハンバーガーメニュー
 const hambBtn = document.querySelector('.hamb-btn');
 const topBar = document.querySelector('.top-bar');
 const middleBar = document.querySelector('.middle-bar');
@@ -21,4 +22,64 @@ hambOverlay.addEventListener('click', () => {
   bottomBar.classList.toggle('active');
   hambOverlay.classList.toggle('active');
   hambMenu.classList.toggle('active');
+});
+
+// スムーススクロール
+// headerの高さ
+const headerHeight = 80;
+// aタグのうち、href属性が#から始まるものを全て取得
+const links = document.querySelectorAll('a[href^="#"]');
+console.log(links);
+
+// linksの要素それぞれにクリックイベントを指定
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener('click', (e) => {
+    // デフォルトの動作をキャンセル
+    e.preventDefault();
+    // href属性の値を取得（#～）
+    const href = links[i].getAttribute('href');
+    // hrefを使ってリンク先の要素を取得
+    const target = document.getElementById(href.replace('#', ''));
+    // リンク先の要素のブラウザの上端からの距離、スクロール量、headerの高さを使ってスクロール先の位置を取得
+    const targetPosition = target.getBoundingClientRect().top + window.scrollY - headerHeight;
+    console.log(target.getBoundingClientRect().top);
+    console.log(window.scrollY);
+    console.log(headerHeight);
+
+    window.scrollTo({
+      top: targetPosition,
+      behavior: 'smooth',
+    });
+  });
+}
+
+
+// PICK UP のカルーセルスライダー
+$('.pickup-items').slick({
+  arrows: false,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  centerMode: true,
+  centerPadding: '8%',
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
+
+// FEATURE の画像フェードイン
+document.addEventListener('scroll', () => {
+  const featureImg = document.querySelectorAll('.feature-img');
+  const margin = 200;
+
+  for (let i = 0; i < featureImg.length; i++) {
+    if (window.innerHeight > featureImg[i].getBoundingClientRect().top + margin) {
+      featureImg[i].classList.add('on');
+    }
+  }
 });
